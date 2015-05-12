97999156db11abd9220762cbc211874c2593ad1a
Sung Jin Kim <sjkim0421@gmail.com>
2015-05-12 11:58:18 -0400

    Create Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,38 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      
-
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def new
+    @user = User.new
+    render :new
   end
 
   def show
+    @user = User.find_by(params[:id])
+    redirect_to user_url(@user)
   end
 
   def edit
+    @user = User.find_by(params[:id])
+    render :new
   end
 
   def update
+    @user = User.find_by(params[:id])
+    if @user.update(user_params)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
-
 
   private
     def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ActiveRecord::Base
 
   has_many :session_tokens
 
-
   def self.generate_random_token
     SecureRandom.urlsafe_base64(16)
   end
@@ -15,6 +14,12 @@ class User < ActiveRecord::Base
   def self.bind_by_credentials(username, password)
     user = User.find_by(username: username)
     user && user.is_password?(password) ? user: nil
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_random_token
+    self.save!
+    self.session_token
   end
 
   def ensure_session_token
