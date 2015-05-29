30dd66cbbebdd4b464f3b2ecec536aa5a0b4de54
Sung Jin Kim <sjkim0421@gmail.com>
2015-05-29 16:18:00 -0400

    add comma after bucket options

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Genius
 
     config.paperclip_defaults = {
       :storage => :s3,
-      :bucket => ENV["s3_bucket"]
+      :bucket => ENV["s3_bucket"],
       :s3_credentials => {
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"]
