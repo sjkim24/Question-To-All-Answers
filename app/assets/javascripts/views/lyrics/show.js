73667a8dd2d3890fbed457d21696a75a6c18bcb1
Sung Jin Kim <sjkim0421@gmail.com>
2016-01-17 20:36:00 -0500

    respace anno show and add border-left to annos

--- app/assets/javascripts/views/lyrics/show.js
+++ app/assets/javascripts/views/lyrics/show.js
@@ -119,7 +119,7 @@ Genius.Views.LyricShow = Backbone.View.extend ({
     this.clearPage();
     event.preventDefault();
     var id = $(event.currentTarget).attr("data-id");
-    var top = $('[data-id=' + id + ']').offset().top - 305;
+    var top = $('[data-id=' + id + ']').offset().top - 342.5;
     var annotation = this.annotations.getOrFetch(id);
     var annoShow = new Genius.Views.AnnotationShow ({
       $rootEl: this.$rootEl,
